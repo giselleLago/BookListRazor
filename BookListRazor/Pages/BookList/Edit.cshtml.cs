6ff05643fdcd742ea7062644b52f39fe42c52c0a
Giselle <gisellelago25994@gmail.com>
2020-05-28 12:11:07 +0200

    Revert "Revert "change Validation Scripts.""
    This reverts commit 277da1436b5bf4dd5ab9a3bd7d7590d1e481be81.

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -8,34 +8,36 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookListRazor.Pages.BookList
 {
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
-        private readonly ApplicationDbContext _db;
+        private ApplicationDbContext _db;
 
-        public CreateModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db)
         {
             _db = db;
         }
 
         [BindProperty]
         public Book Book { get; set; }
-        public void OnGet()
-        {
 
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
         }
 
         public async Task<IActionResult> OnPost()
         {
             if (ModelState.IsValid)
             {
-                await _db.Book.AddAsync(Book);
+                var BookFromDb = await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.ISBN = Book.ISBN;
+
                 await _db.SaveChangesAsync();
                 return RedirectToPage("Index");
             }
-            else
-            {
-                return Page();
-            }
+            return RedirectToPage();
         }
     }
 }
